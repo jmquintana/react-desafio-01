15c1e18b510886e3ad9d15f736cf4e6e9b4aef9e
jmquintana <mailjmq@gmail.com>
2023-08-14 23:47:14 -0300

    deps installed

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,26 @@ import styles from "./App.module.scss";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer.jsx";
 
 function App() {
-	const inputName = useRef(null);
-	const inputAge = useRef(null);
+	// const inputName = useRef(null);
+	// const inputAge = useRef(null);
 
-	const handleOnSubmit = (e) => {
-		e.preventDefault();
-		const name = inputName.current.value;
-		const age = inputAge.current.value;
-		console.log(name, age);
-	};
+	// const handleOnSubmit = (e) => {
+	// 	e.preventDefault();
+	// 	const name = inputName.current.value;
+	// 	const age = inputAge.current.value;
+	// 	console.log(name, age);
+	// };
 
 	return (
 		<div className={styles.appContainer}>
-			{/* <nav className={styles.navContainer}>
+			<nav className={styles.navContainer}>
 				<NavBar />
 			</nav>
 			<main className={styles.mainContainer}>
 				<ItemListContainer greeting="Bienvenidos a mi tienda..." />
 			</main>
 			<ItemListContainer greeting="Bienvenido a mi tienda..."></ItemListContainer>
-			<footer className={styles.footerContainer}>Footer</footer> */}
+			<footer className={styles.footerContainer}>Footer</footer>
 		</div>
 	);
 }
--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,6 +1,7 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
-import styles from "./CartWidget.module.scss";
+import styles from "./CartWidget.scss?inline";
+
 const CartWidget = () => {
 	return (
 		<div className={styles.cartContainer}>
